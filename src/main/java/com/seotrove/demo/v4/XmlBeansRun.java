a68563c1fdf4f00dcfda76b477641dddcd709c41
muhammadismailkhan0009 <muhammadismailkhan0009@gmail.com>
2025-10-14 11:58:04 +0500

    - added xml beans logic

--- src/main/java/com/seotrove/demo/v4/XmlBeansRun.java
+++ src/main/java/com/seotrove/demo/v4/XmlBeansRun.java
@@ -1,16 +1,91 @@
 package com.seotrove.demo.v4;
 
 
+import au.gov.sbr.ato.iitr.IdentifiersAustralianBusinessNumberIdentifierDocument;
+import org.apache.xmlbeans.XmlCursor;
+import org.apache.xmlbeans.XmlError;
+import org.apache.xmlbeans.XmlObject;
+import org.apache.xmlbeans.XmlOptions;
+import org.w3.x1999.xlink.TypeAttribute;
 import org.xbrl.x2003.instance.XbrlDocument;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XmlBeansRun {
 
     public static void main(String[] args) throws Exception {
-        var xbrl = XbrlDocument.Xbrl.Factory.newInstance();
+      var xbrlDocument = createXbrlDocument();
+      var xbrl = createXbrl(xbrlDocument);
+      var options = setOptions();
 
+      addIntermediaryDetails(xbrl);
+      save("sample_iitr.xml", xbrlDocument, options);
+
+      validate(xbrl, options);
 
     }
 
-    public void validate(){}
-    public static void createXbrlDocument(){}
+    public static void addIntermediaryDetails(XbrlDocument.Xbrl xbrl) {
+        var businessNumberDoc = IdentifiersAustralianBusinessNumberIdentifierDocument.Factory.newInstance();
+
+        var businessNumber = businessNumberDoc.addNewIdentifiersAustralianBusinessNumberIdentifier();
+        businessNumber.setStringValue("11111111111");
+        businessNumber.setContextRef("intermediary_context");
+        addElementToXMl(businessNumberDoc,xbrl);
+    }
+
+    public static void save(String outputFilePath, XbrlDocument xbrlDocument, XmlOptions opts) throws Exception {
+        xbrlDocument.save(new File(outputFilePath), opts);
+    }
+
+
+    public static XmlOptions setOptions(){
+        XmlOptions opts = new XmlOptions();
+        opts.setSavePrettyPrint();
+        opts.setSaveAggressiveNamespaces();
+        opts.setValidateStrict();
+
+        return opts;
+    }
+    public static void addElementToXMl(XmlObject element, XbrlDocument.Xbrl xbrl) {
+        try (XmlCursor rootCursor = xbrl.newCursor();
+             XmlCursor famCursor = element.newCursor()) {
+
+            famCursor.toFirstChild();
+
+            rootCursor.toEndToken(); // move inside <xbrl>
+            famCursor.copyXml(rootCursor); // copy element subtree
+        }
+    }
+
+    public static void validate(XbrlDocument.Xbrl xbrl, XmlOptions options) throws Exception {
+
+        // Validation error listener
+        List<XmlError> errors = new ArrayList<>();
+        options.setErrorListener(errors);
+
+        if (!xbrl.validate(options)) {
+            System.out.println("❌ Validation failed for XBRL root:");
+            for (XmlError err : errors) {
+                System.out.println("  - " + err );
+            }
+            return; // Stop if invalid
+        }
+    }
+
+    public static XbrlDocument.Xbrl createXbrl(XbrlDocument xbrlDoc) throws Exception {
+        var xbrl = xbrlDoc.addNewXbrl();
+       var schemaRef = xbrl.addNewSchemaRef();
+       schemaRef.setType(TypeAttribute.Type.Enum.forString("simple"));
+       schemaRef.setHref("a.com");
+
+        return xbrl;
+    }
+    public static XbrlDocument createXbrlDocument(){
+      var  xbrl = XbrlDocument.Factory.newInstance();
+
+      return xbrl;
+    }
 }
